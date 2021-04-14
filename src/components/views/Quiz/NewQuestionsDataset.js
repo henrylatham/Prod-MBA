73a650d542ad6103c4880f25f7ee1cae06f775da
Henry Latham <henrylatham5555@gmail.com>
2021-04-14 06:35:51 -0300

    complete dataset with 20 questions and answers

--- src/components/views/Quiz/NewQuestionsDataset.js
+++ src/components/views/Quiz/NewQuestionsDataset.js
@@ -8,12 +8,12 @@ export const NewQuestionsDataset = [
     answers: [
       // You can put as much you want. Just one or 999 answers. Pay attention that all fields are populated ( answer and points )
       {
-        answer: 'Affiliate model',
-        points: 1,
+        answer: 'Affiliate Model',
+        points: 0,
       },
       {
-        answer: 'Cast and return model',
-        points: 0,
+        answer: 'Cast and Return Model',
+        points: 2,
       },
       {
         answer: 'Leasing model',
@@ -21,7 +21,408 @@ export const NewQuestionsDataset = [
       },
       {
         answer: 'Substantive model',
-        points: 5,
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'Which of the following is not a business model:',
+    order: 2,
+    type: 'productStrategy',
+    answers: [
+      {
+        answer: 'Franchise Model',
+        points: 0,
+      },
+      {
+        answer: 'Brokerage Model',
+        points: 0,
+      },
+      {
+        answer: 'Leasing Model',
+        points: 0,
+      },
+      {
+        answer: 'Substantive Model',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'Which statement best describes what a product strategy is?',
+    order: 3,
+    type: 'productStrategy',
+    answers: [
+      {
+        answer: 'A good plan',
+        points: 0,
+      },
+      {
+        answer: 'A framework for helping you make decisions',
+        points: 2,
+      },
+      {
+        answer: 'A detailed roadmap & well-defined backlog',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'People I have managed would say I inspire them:',
+    order: 4,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'Strongly disagree',
+        points: 0,
+      },
+      {
+        answer: 'Disagree',
+        points: 0,
+      },
+      {
+        answer: 'Unsure',
+        points: 0,
+      },
+      {
+        answer: 'Agree',
+        points: 1,
+      },
+      {
+        answer: 'Strongly agree',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question:
+      'Which of the following are important considerations when motivating a team?',
+    order: 5,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'How you can make them work harder',
+        points: 0,
+      },
+      {
+        answer: 'What their individual goals are',
+        points: 1,
+      },
+      {
+        answer: 'Creating open lines of communication',
+        points: 1,
+      },
+      {
+        answer: 'Their learning styles',
+        points: 1,
+      },
+    ],
+  },
+  {
+    question:
+      'What is the best way to navigate a difficult stakeholder when prioritising work?',
+    order: 6,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'Exert your authority as the final decision maker',
+        points: 0,
+      },
+      {
+        answer:
+          'Identify their motivation and help them understand your process & decisions',
+        points: 2,
+      },
+      {
+        answer:
+          'Squeeze their work in when the developers have time between tasks',
+        points: 0,
+      },
+      {
+        answer: 'Do what they want if they are more senior',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'What is the best way to navigate a difficult stakeholder when prioritising work?',
+    order: 7,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'Exert your authority as the final decision maker',
+        points: 0,
+      },
+      {
+        answer:
+          'Identify their motivation and help them understand your process & decisions',
+        points: 2,
+      },
+      {
+        answer:
+          'Squeeze their work in when the developers have time between tasks',
+        points: 0,
+      },
+      {
+        answer: 'Do what they want if they are more senior',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'I know how to launch a new product or feature for maximum success:',
+    order: 8,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'Strongly disagree',
+        points: 0,
+      },
+      {
+        answer: 'Disagree',
+        points: 0,
+      },
+      {
+        answer: 'Unsure',
+        points: 0,
+      },
+      {
+        answer: 'Agree',
+        points: 1,
+      },
+      {
+        answer: 'Strongly agree',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'When launching a new product, what would you do first?',
+    order: 9,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'Fundraise for the project (e.g. investors or kickstarter)',
+        points: 0,
+      },
+      {
+        answer: 'Create a landing page to gather emails',
+        points: 0,
+      },
+      {
+        answer:
+          'Speak to people in my network that are within the target niche',
+        points: 2,
+      },
+      {
+        answer: 'Build a prototype ',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'How would you determine whether a feature was successful or not?',
+    order: 10,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'Rapid delivery according to preagreed specifications',
+        points: 0,
+      },
+      {
+        answer: 'Ability of the user to complete the user story',
+        points: 0,
+      },
+      {
+        answer: 'Delivery of expected customer & business value',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'I know what my customers want:',
+    order: 11,
+    type: 'customerInsight',
+    answers: [
+      {
+        answer: 'Strongly disagree',
+        points: 0,
+      },
+      {
+        answer: 'Disagree',
+        points: 0,
+      },
+      {
+        answer: 'Unsure',
+        points: 0,
+      },
+      {
+        answer: 'Agree',
+        points: 1,
+      },
+      {
+        answer: 'Strongly agree',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'How would you best define your customer wants?',
+    order: 12,
+    type: 'customerInsight',
+    answers: [
+      {
+        answer: 'Analysis of demographic data (e.g. age)',
+        points: 0,
+      },
+      {
+        answer:
+          'Analysis of psychographic traits (e.g. desire to be respected by others)',
+        points: 2,
+      },
+      {
+        answer: 'Using existing assumptions within your company',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'How would you validate a new product or feature idea?',
+    order: 13,
+    type: 'customerInsight',
+    answers: [
+      {
+        answer: 'Speak to people at my company/on my team',
+        points: 1,
+      },
+      {
+        answer: 'Speak to friends & family who I think would use my product',
+        points: 1,
+      },
+      {
+        answer: 'Research competitors in the space to see what they are doing',
+        points: 1,
+      },
+      {
+        answer: 'Speak to a customer that uses a competitor',
+        points: 1,
+      },
+    ],
+  },
+
+  //
+  // True or False Questions
+  // Type options: productStrategy, execution, influencer, customerInsight
+
+  {
+    question:
+      'I must have a business plan in place before I start validating my niche',
+    order: 14,
+    type: 'productStrategy',
+    answers: [
+      {
+        answer: 'True',
+        points: 0,
+      },
+      {
+        answer: 'False',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question:
+      'If I need to pivot my idea early on, I did not carry out the correct research',
+    order: 15,
+    type: 'customerInsight',
+    answers: [
+      {
+        answer: 'True',
+        points: 0,
+      },
+      {
+        answer: 'False',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'A great product should be driven by one unique customer insight',
+    order: 16,
+    type: 'customerInsight',
+    answers: [
+      {
+        answer: 'True',
+        points: 2,
+      },
+      {
+        answer: 'False',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'Managing my ability to stay objective & to persist when assessing an opportunity is as important as using the most up-to-date frameworks & processes',
+    order: 17,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'True',
+        points: 2,
+      },
+      {
+        answer: 'False',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'I can objectively determine when to pivot or persist ',
+    order: 18,
+    type: 'influencer',
+    answers: [
+      {
+        answer: 'True',
+        points: 0,
+      },
+      {
+        answer: 'False',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question: 'Defining product vision is key before defining my solution',
+    order: 19,
+    type: 'productStrategy',
+    answers: [
+      {
+        answer: 'True',
+        points: 0,
+      },
+      {
+        answer: 'False',
+        points: 2,
+      },
+    ],
+  },
+  {
+    question:
+      'My team should never build something unless they are clear on what they expect the outcome to be',
+    order: 20,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'True',
+        points: 2,
+      },
+      {
+        answer: 'False',
+        points: 0,
       },
     ],
   },
