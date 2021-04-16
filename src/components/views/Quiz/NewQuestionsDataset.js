fc1d862978ccb3268cd1be6d09dd5df6f4152d1d
Tomislav Horaček <tomislav.horacek@thespian.hr>
2021-04-16 18:31:15 +0200

    Introduce a new Quiz logic

---
+++ src/components/views/Quiz/NewQuestionsDataset.js
@@ -0,0 +1,97 @@
+// @TODO - Henry to Define Quiestions and Questions sections
+
+export const NewQuestionsDataset = [
+  {
+    question: 'Question 1 - productStrategy',
+    order: 1, // You can change question appeareance by changing order number
+    type: 'productStrategy', // One of productStrategy, execution, influencer, customerInsight
+    answers: [
+      // You can put as much you want. Just one or 999 answers. Pay attention that all fields are populated ( answer and points )
+      {
+        answer: 'Affiliate model 1',
+        points: 1,
+      },
+      {
+        answer: 'Cast and return model 0',
+        points: 0,
+      },
+      {
+        answer: 'Leasing model 0',
+        points: 0,
+      },
+      {
+        answer: 'Substantive model 0',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'Question 3 - productStrategy',
+    order: 3,
+    type: 'productStrategy',
+    answers: [
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 1',
+        points: 1,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'Question 2 - execution',
+    order: 2,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'Answer - 1',
+        points: 1,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+    ],
+  },
+  {
+    question: 'Question 4 - execution',
+    order: 4,
+    type: 'execution',
+    answers: [
+      {
+        answer: 'Answer - 1',
+        points: 1,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+      {
+        answer: 'Answer - 0',
+        points: 0,
+      },
+    ],
+  },
+];
